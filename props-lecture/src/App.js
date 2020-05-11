aaef0c9690747b01663866323f2c9b54dfa13d90
sjproctor <sproctor950@gmail.com>
2020-05-11 14:55:01 -0700

    adds comments to the props lectures

--- props-lecture/src/App.js
+++ props-lecture/src/App.js
@@ -7,13 +7,18 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
+      // grocery list items
       listItems: ["apples", "yogurt", "cheese", "blueberries", "ice cream"],
+      // hardware list items
       hardwareItems: ["lightbulb", "nails", "plant food"],
+      // random chore list
       wildCardChore: ["Laundry", "Sweep", "Dust", "Vacuum", "Make bed"],
+      // selection of just one chore
       dailyChore: ""
     }
   }
 
+  // method that will select one chore and update the value of dailyChore in state
   addChore = () => {
     let random = Math.floor(Math.random() * this.state.wildCardChore.length)
     this.setState({ dailyChore: this.state.wildCardChore[random] })
@@ -23,7 +28,9 @@ class App extends Component{
     return(
       <React.Fragment>
         <h1>To Do List</h1>
+        {/* Calling the Item component and passing in two values as props: 1) listItems from state, 2) a string */}
         <Item list={ this.state.listItems } store="Grocery"/>
+        {/* Same thing as above, but passing in different values */}
         <Item list={ this.state.hardwareItems } store="Hardware"/>
         <WildCard dailyChore={ this.state.dailyChore }/>
         <button onClick={ this.addChore }>Add Chore</button>
--- props-lecture/src/components/Item.js
+++ props-lecture/src/components/Item.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react'
 class Item extends Component{
 
   render(){
-
+    // list is an array that comes from App as props
     let eachItem = this.props.list.map(item => {
       return(
+        // each item can be returned as a JSX list item
         <li>{ item }</li>
       )
     })
@@ -13,7 +14,9 @@ class Item extends Component{
 
     return(
       <React.Fragment>
+      {/* store comes from App as props */}
         <h3>{ this.props.store} Items</h3>
+        {/* each item from the array gets passed into the unordered list */}
         <ul>
           { eachItem }
         </ul>
