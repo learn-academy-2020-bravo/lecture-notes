7bffc46af69be9fe199a9dcf88b46ac393faebe7
sjproctor <sproctor950@gmail.com>
2020-05-11 18:51:49 -0700

    adds comments to functional props

--- functional-props/src/App.js
+++ functional-props/src/App.js
@@ -5,11 +5,13 @@ import './App.css'
 class App extends Component{
   constructor(props){
     super(props)
+    // state object that holds the one daily chore that is selected from the array of options in WildCard
     this.state = {
       dailyChore: ""
     }
   }
 
+  // this method gets passed as props to WildCard and gets called by a method in WildCard which passes an argument of one chore from the array
   addDailyChore = (chore) => {
     this.setState({ dailyChore: chore })
   }
@@ -19,6 +21,7 @@ class App extends Component{
     return(
       <React.Fragment>
         <h1>To Do App</h1>
+        {/* Wild Card component call, passing two values as props*/}
         <WildCard
           addDailyChore={ this.addDailyChore }
           dailyChore={ this.state.dailyChore }
--- functional-props/src/components/WildCard.js
+++ functional-props/src/components/WildCard.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react'
 
 class WildCard extends Component{
 
+  // method called onClick
   addChore = () => {
+    // array of possible chores
     let wildCardChore = ["Laundry", "Sweep", "Dust"]
+    // creating a random number
     let random = Math.floor(Math.random() * wildCardChore.length)
+    // calling the method in App.js and passing a random chore as an argument
     this.props.addDailyChore(wildCardChore[random])
   }
 
@@ -13,7 +17,9 @@ class WildCard extends Component{
     return(
       <React.Fragment>
         <h3>Wild Card</h3>
+        {/* receiving dailyChore as props from App.js */}
         <p>Today's chore: { this.props.dailyChore }</p>
+        {/* calling the addChore method */}
         <button onClick={ this.addChore }>Add Chore</button>
       </React.Fragment>
     )
